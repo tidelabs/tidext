2115b965b7e0fb811a358c64e79d4d4e2744aa46
David <david@lemarier.ca>
2022-03-30 12:30:05 +0000

    add license and prepare for initial release

--- examples/examples/local.rs
+++ examples/examples/local.rs
@@ -1,7 +1,22 @@
+// Copyright 2021-2022 Semantic Network Ltd.
+// This file is part of tidext.
+
+// tidext is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// Tidechain is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with tidext.  If not, see <http://www.gnu.org/licenses/>.
+
 use tidext::{ClientBuilder, TidefiKeyring};
 
 // load sr25519 test account
-
 use sp_keyring::AccountKeyring;
 
 #[path = "../src/lib.rs"]
--- macro/src/lib.rs
+++ macro/src/lib.rs
@@ -1,3 +1,19 @@
+// Copyright 2021-2022 Semantic Network Ltd.
+// This file is part of tidext.
+
+// tidext is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// Tidechain is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with tidext.  If not, see <http://www.gnu.org/licenses/>.
+
 use proc_macro::{self, TokenStream};
 use syn::spanned::Spanned;
 
@@ -8,7 +24,7 @@ mod utils;
 #[proc_macro_attribute]
 pub fn tidext(attr: TokenStream, item: TokenStream) -> TokenStream {
   if !attr.is_empty() {
-    let msg = "Invalid pallet macro call: expected no attributes, e.g. macro call must be just \
+    let msg = "Invalid tidext macro call: expected no attributes, e.g. macro call must be just \
 			``#[tidext]`";
     let span = proc_macro2::TokenStream::from(attr).span();
     return syn::Error::new(span, msg).to_compile_error().into();
