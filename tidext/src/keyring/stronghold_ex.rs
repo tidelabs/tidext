f227bbbff5a9d70ebd433cae9c7c0cc719c2d279
Tensor <tensor@semantic-network.com>
2022-04-05 16:10:54 +0000

    Fix client_ex.

--- tidext/src/keyring/stronghold_ex.rs
+++ tidext/src/keyring/stronghold_ex.rs
@@ -14,7 +14,8 @@
 // You should have received a copy of the GNU General Public License
 // along with tidext.  If not, see <http://www.gnu.org/licenses/>.
 
-use crate::{keyring::AccountId, TidechainConfig};
+use crate::{AccountId, TidechainConfig};
+
 use std::fmt;
 use subxt::{
   sp_core::{
