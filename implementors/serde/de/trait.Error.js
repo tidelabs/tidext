5ce0eb59ea6984f7aafaeba7a180b962fa89bf79
tidechain-ci <tidechain.ci@gmail.com>
2022-06-11 10:15:59 +0000

    Updated docs for dev

--- implementors/serde/de/trait.Error.js
+++ implementors/serde/de/trait.Error.js
@@ -1,5 +1,6 @@
 (function() {var implementors = {};
 implementors["bincode"] = [{"text":"impl <a class=\"trait\" href=\"serde/de/trait.Error.html\" title=\"trait serde::de::Error\">Error</a> for <a class=\"type\" href=\"bincode/type.Error.html\" title=\"type bincode::Error\">Error</a>","synthetic":false,"types":["bincode::error::Error"]}];
+implementors["scale_value"] = [{"text":"impl <a class=\"trait\" href=\"serde/de/trait.Error.html\" title=\"trait serde::de::Error\">Error</a> for <a class=\"struct\" href=\"scale_value/serde/struct.DeserializerError.html\" title=\"struct scale_value::serde::DeserializerError\">DeserializerError</a>","synthetic":false,"types":["scale_value::serde_impls::deserializer::DeserializerError"]}];
 implementors["serde"] = [];
 implementors["serde_json"] = [{"text":"impl <a class=\"trait\" href=\"serde/de/trait.Error.html\" title=\"trait serde::de::Error\">Error</a> for <a class=\"struct\" href=\"serde_json/struct.Error.html\" title=\"struct serde_json::Error\">Error</a>","synthetic":false,"types":["serde_json::error::Error"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
